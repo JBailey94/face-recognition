3d65f168461b855aae0ee79ac5f3c5ef46823fb4
JBailey94 <jessebailey99@gmail.com>
2020-05-14 21:13:05 -0500

    Updates

---
+++ precache-manifest.bfb681a4a83ad3ddc071f71b52768ef5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c20e813ba582b6c84267b67f4837cd8",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "8e6b971ec74ccb7875c3",
+    "url": "/face-recognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0bb1fa70ce89de3bc736",
+    "url": "/face-recognition/static/css/main.2d023210.chunk.css"
+  },
+  {
+    "revision": "8e6b971ec74ccb7875c3",
+    "url": "/face-recognition/static/js/2.7312cb3e.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recognition/static/js/2.7312cb3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bb1fa70ce89de3bc736",
+    "url": "/face-recognition/static/js/main.b37c015c.chunk.js"
+  },
+  {
+    "revision": "fa1bc4860708b895953c",
+    "url": "/face-recognition/static/js/runtime-main.976500cc.js"
+  }
+]);
